3fe5b84db337f13315583f34ff516b58f574c02a
agranom <boyko330@gmail.com>
2023-07-14 18:28:47 +0300

    + actions menu

--- src/components/grocery/GroceryItem.tsx
+++ src/components/grocery/GroceryItem.tsx
@@ -1,4 +1,15 @@
-import { Card, CardContent, Checkbox, FormControlLabel } from '@mui/material';
+import { Delete, Edit, MoreVert } from '@mui/icons-material';
+import {
+    Card,
+    CardContent,
+    Checkbox,
+    FormControlLabel,
+    IconButton,
+    ListItemIcon,
+    ListItemText,
+    Menu,
+    MenuItem,
+} from '@mui/material';
 import React from 'react';
 import { eGroceryItemStatus, IGroceryItem } from '../../models/grocery-item';
 import { GroceryTypesTranslator } from '../../utils/grocery-types-translator';
@@ -8,6 +19,14 @@ const GroceryItem: React.FC<{ item: IGroceryItem, onItemChange: (item: Partial<I
                                                                                                                 item,
                                                                                                                 onItemChange,
                                                                                                             }) => {
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleMenuClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const status = event.target.checked ? eGroceryItemStatus.Done : eGroceryItemStatus.Undone;
         onItemChange({ status, id: item.id });
@@ -25,6 +44,25 @@ const GroceryItem: React.FC<{ item: IGroceryItem, onItemChange: (item: Partial<I
                                   label={item.name}/>
                 <p className={styles.groceryItemPriority + ' ' + styles[item.priority]}>{GroceryTypesTranslator.toItemPriority(item.priority)}</p>
             </CardContent>
+            <div className={styles.groceryItemActions}>
+                <IconButton onClick={handleMenuClick}>
+                    <MoreVert/>
+                </IconButton>
+                <Menu open={open} anchorEl={anchorEl} onClose={handleMenuClose}>
+                    <MenuItem onClick={handleMenuClose}>
+                        <ListItemIcon>
+                            <Delete/>
+                        </ListItemIcon>
+                        <ListItemText>Удалить</ListItemText>
+                    </MenuItem>
+                    <MenuItem onClick={handleMenuClose}>
+                        <ListItemIcon>
+                            <Edit/>
+                        </ListItemIcon>
+                        <ListItemText>Редактировать</ListItemText>
+                    </MenuItem>
+                </Menu>
+            </div>
         </Card>
     );
 };
