07124391a5a9a3d4fe6374c0446353848b107e57
Vitalii Boiko <boyko330@gmail.com>
2026-01-28 11:25:41 +0200

    ! fix nullable recipeId API calls

--- src/features/grocery/RecipeGroceries.tsx
+++ src/features/grocery/RecipeGroceries.tsx
@@ -1,7 +1,4 @@
 import { IGroceryItem } from './models/grocery-item';
-import styles from './GroceryCategory.module.scss';
-import { useRecipeById } from '../recipe/api/get-recipe';
-import GroceriesList from './GroceriesList';
 import { groupBy } from 'lodash';
 import RecipeGroceryGroup from './RecipeGroceryGroup';
 
@@ -10,7 +7,7 @@ type RecipeGroceriesProps = {
 };
 
 const RecipeGroceries = ({ groceries }: RecipeGroceriesProps) => {
-    const groupedByRecipe = groupBy(groceries, 'recipeId')
+    const groupedByRecipe = groupBy(groceries.filter(g => !!g.recipeId), 'recipeId')
 
     return <>
         {Object.entries(groupedByRecipe)
